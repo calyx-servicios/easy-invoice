c64051a54aacfa84a10ab819b6b36461aa92dd20
cristian <crisdparadiso@gmail.com>
2022-06-03 19:30:17 -0300

    [ODOO-3964][IMP][easy_invoice_cash] fields date from-to required now date filters use <= and >= remove 3 empty lines add column width

--- easy_invoice_cash/models/easy_recaudation_xlsx.py
+++ easy_invoice_cash/models/easy_recaudation_xlsx.py
@@ -9,6 +9,16 @@ class EasyRecaudationXlsx(models.AbstractModel):
         report_name = partners.name
         sheet = workbook.add_worksheet(_("Cashbox Report"))
         bold = workbook.add_format({'bold': True})
+        sheet.set_column(0, 0, 11)
+        sheet.set_column(1, 1, 20)
+        sheet.set_column(2, 2, 8)
+        sheet.set_column(3, 3, 15)
+        sheet.set_column(4, 4, 11)
+        sheet.set_column(5, 5, 20)
+        sheet.set_column(6, 6, 25)
+        sheet.set_column(7, 7, 8)
+        sheet.set_column(8, 8, 8)
+
         sheet.write(0, 0, _("Date"), bold)
         sheet.write(0, 1, _("Account Move"), bold)
         sheet.write(0, 2, _("Journal"), bold)
@@ -18,9 +28,9 @@ class EasyRecaudationXlsx(models.AbstractModel):
         sheet.write(0, 6, _("Sequence"), bold)
         sheet.write(0, 7, _("State"), bold)
         sheet.write(0, 8, _("Balance"), bold)
-        line = 4
+        line = 1
         for aml in partners.move_ids:
-            if partners.report_date_from < aml.date < partners.report_date_to:
+            if partners.report_date_from <= aml.date <= partners.report_date_to:
                 sheet.write(line, 0, aml.date)
                 sheet.write(line, 1, aml.move_id.name)
                 sheet.write(line, 2, aml.journal_id.name)
@@ -32,7 +42,7 @@ class EasyRecaudationXlsx(models.AbstractModel):
                 sheet.write(line, 8, aml.balance)
                 line += 1
         for easy_aml in partners.line_recaudation_ids:
-            if partners.report_date_from < easy_aml.date_pay < partners.report_date_to:
+            if partners.report_date_from <= easy_aml.date_pay <= partners.report_date_to:
                 sheet.write(line, 0, easy_aml.date_pay)
                 sheet.write(line, 1, easy_aml.invoice_id.name)
                 sheet.write(line, 2, "-")
